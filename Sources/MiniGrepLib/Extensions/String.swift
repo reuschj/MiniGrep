98120d7f25c1bf06f9629e00d7f55c26db1f91c4
reuschj <reuschj@gmail.com>
2020-10-03 02:44:34 -0500

    Switch to maps

--- Sources/MiniGrepLib/Extensions/String.swift
+++ Sources/MiniGrepLib/Extensions/String.swift
@@ -3,9 +3,17 @@ extension String {
     /// Wraps the string with escapes to produce the given terminal color
     public func terminalColor(_ color: TerminalColor) -> Self { color.wrap(self) }
 
+    /// Gets ranges of the given search query within the string (or inverted ranges)
     public func getRanges<Query: StringProtocol>(of query: Query, inverted: Bool = false) -> [Range<String.Index>]? {
         var rangeIterator = QueryRangeIterator(String(query), in: self, inverted: inverted)
         let ranges = rangeIterator.collect()
+        return ranges.isEmpty ? nil : ranges
+    }
+    
+    /// Maps ranges of the given search query within the string (or inverted ranges)
+    public func mapRanges<Query: StringProtocol, R>(of query: Query, inverted: Bool = false, _ transform: (Range<String.Index>) -> R) -> [R]? {
+        var rangeIterator = QueryRangeIterator(String(query), in: self, inverted: inverted)
+        let ranges = rangeIterator.map(transform)
         return ranges.isEmpty ? nil : ranges
     }
 }
